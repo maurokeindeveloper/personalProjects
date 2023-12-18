6086caed8abaa2a0739b0f25c87c37e95d835d41
maurokeindeveloper <maurokeindeveloper@gmail.com>
2023-12-18 15:28:50 -0300

    lot of new personal projects

---
+++ python/mouredev/frontend/python-web-curso/.web/pages/index.js
@@ -0,0 +1,27 @@
+
+/** @jsxImportSource @emotion/react */import { Fragment } from "react"
+import { Fragment_fd0e7cb8f9fb4669a6805377d925fba0 } from "/utils/stateful_components"
+import { Text } from "@chakra-ui/react"
+import "focus-visible/dist/focus-visible"
+import NextHead from "next/head"
+
+
+
+export default function Component() {
+
+  return (
+    <Fragment>
+  <Fragment_fd0e7cb8f9fb4669a6805377d925fba0/>
+  <Text sx={{"color": "blue"}}>
+  {`Hola Reflex!`}
+</Text>
+  <NextHead>
+  <title>
+  {`Reflex App`}
+</title>
+  <meta content={`A Reflex app.`} name={`description`}/>
+  <meta content={`favicon.ico`} property={`og:image`}/>
+</NextHead>
+</Fragment>
+  )
+}
