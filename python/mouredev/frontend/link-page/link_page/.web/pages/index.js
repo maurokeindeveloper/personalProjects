86741667b9d44bf57cb4cfe958b9b154a09b1171
maurokeindeveloper <maurokeindeveloper@gmail.com>
2023-12-18 21:21:24 -0300

    working with reflex first steps

---
+++ python/mouredev/frontend/link-page/link_page/.web/pages/index.js
@@ -0,0 +1,29 @@
+
+/** @jsxImportSource @emotion/react */import { Fragment } from "react"
+import { Fragment_fd0e7cb8f9fb4669a6805377d925fba0 } from "/utils/stateful_components"
+import { HStack, Text } from "@chakra-ui/react"
+import "focus-visible/dist/focus-visible"
+import NextHead from "next/head"
+
+
+
+export default function Component() {
+
+  return (
+    <Fragment>
+  <Fragment_fd0e7cb8f9fb4669a6805377d925fba0/>
+  <HStack sx={{"position": "sticky", "bg": "blue", "paddingX": "16px", "paddingY": "8px", "zIndex": "999"}}>
+  <Text sx={{"height": "40px"}}>
+  {`Mauro Kein`}
+</Text>
+</HStack>
+  <NextHead>
+  <title>
+  {`Reflex App`}
+</title>
+  <meta content={`A Reflex app.`} name={`description`}/>
+  <meta content={`favicon.ico`} property={`og:image`}/>
+</NextHead>
+</Fragment>
+  )
+}
